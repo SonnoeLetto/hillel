183c516bc4e119a11b3fe8302f92d9f793b3bb4d
sdon <sdonchenko@outlook.com>
2021-01-02 19:40:09 +0200

    a

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-for (var a = 2; a < 9; a++) {
-     var line = '';
-     for (var b = 2; b <= 9; b++) {
-          line = line + a * b + ', ';
-     }
-     console.log(line);
-}
+// for (var a = 2; a < 9; a++) {
+//      var line = '';
+//      for (var b = 2; b <= 9; b++) {
+//           line = line + a * b + ', ';
+//      }
+//      console.log(line);
+// }
+
+var main = { one: 42 };
+var bain = { one: 32 };
+bain = main;
+var main = { one: 22 };
+console.log(bain)
